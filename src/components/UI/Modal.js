3bc35119256073e8ed52830a116a7e90267d5a0a
Gatarski <grzegorz.gatarski@stxnext.pl>
2021-09-20 11:48:10 +0200

    Portal for modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,23 +1,35 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import Button from "./Button";
 import './Modal.css'
 
-const Modal = (props) => {
+
+const Backdrop = () => {
+  return <div className='backdrop'></div>
+}
+
+const Overlay = (props) => {
   return (
-    <React.Fragment>
-      <div className='backdrop'></div>
-      <div className='modal'>
-        <header className='header'>
-          <h2>{props.title}</h2>
-        </header>
-        <div>
-          <p>{props.message}</p>
-        </div>
-        <footer className='actions'>
-          <Button onClick={props.onClick}>Okay</Button>
-        </footer>
+    <div className='modal'>
+      <header className='header'>
+        <h2>{props.title}</h2>
+      </header>
+      <div>
+        <p>{props.message}</p>
       </div>
-    </React.Fragment>
+      <footer className='actions'>
+        <Button onClick={props.onClick}>Okay</Button>
+      </footer>
+    </div>
+  )
+}
+
+const Modal = (props) => {
+  return(
+    <React.Fragment>
+       {ReactDOM.createPortal(<Backdrop/>, document.getElementById('backdrop-id'))}
+       {ReactDOM.createPortal(<Overlay title={props.title} message={props.message} onClick={props.onClick}/>, document.getElementById('overlay-id'))}
+     </React.Fragment>
   )
 }
 
